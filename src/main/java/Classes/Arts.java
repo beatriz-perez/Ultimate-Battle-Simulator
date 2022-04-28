de99cb622628ab4b0f04ca0fd9630cb51523487d
beatriz-perez <beatriz.pfp@gmail.com>
2022-04-28 11:59:40 +0200

    Adds arts to show in main-menu

---
+++ src/main/java/Classes/Arts.java
@@ -0,0 +1,40 @@
+package Classes;
+
+public class Arts {
+
+    public static void printCastle() {
+        System.out.println("                             -|             |-");
+        System.out.println("         -|                  [-_-_-_-_-_-_-_-]                  |-");
+        System.out.println("         [-_-_-_-_-]          |             |          [-_-_-_-_-]");
+        System.out.println("          | o   o |           [  0   0   0  ]           | o   o |");
+        System.out.println("          | o   o |           [  0   0   0  ]           | o   o |");
+        System.out.println("           |     |    -|       |           |       |-    |     |");
+        System.out.println("           |     |_-___-___-___-|         |-___-___-___-_|     |");
+        System.out.println("           |  o  ]              [    0    ]              [  o  |");
+        System.out.println("           |     ]   o   o   o  [ _______ ]  o   o   o   [     | ----_______");
+        System.out.println("_____----- |     ]              [ ||||||| ]              [     |");
+        System.out.println("           |     ]              [ ||||||| ]              [     |");
+        System.out.println("       _-_-|_____]--------------[_|||||||_]--------------[_____|-_-_");
+        System.out.println("      ( (__________------------_____________-------------_________) )");
+        System.out.println("\n");
+
+    }
+
+    public static void printSwords() {
+        System.out.println("      O                                     O");
+        System.out.println("{o)xxx|===============-  *  -===============|xxx(o}");
+        System.out.println("      O                                     O");
+        System.out.println("\n");
+
+    }
+
+    public static void printTombs() {
+        System.out.println("\t      ,-=-.       ______     _");
+        System.out.println("\t     /  +  \\     />----->  _|R|_");
+        System.out.println("\t     | ~~~ |    // -/- /  |_ I _|");
+        System.out.println("\t     |R.I.P|   //  /  /     |P|");
+        System.out.println("\t\\vV,,|_____|V,//_____/VvV,v,|_|/,,vhjwv/,");
+        System.out.println("\n");
+
+    }
+}
