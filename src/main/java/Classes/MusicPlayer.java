23f0147ea82dc51f3985624389c15024bb982508
Paula Kamisato <paukamisato@gmail.com>
2022-04-28 15:32:44 +0200

    music

---
+++ src/main/java/Classes/MusicPlayer.java
@@ -0,0 +1,25 @@
+
+package Classes;
+
+
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class MusicPlayer {
+
+    public static void musicPlayer(){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("epic_battle_music_1-6275.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            System.out.println("Error with playing sound.");
+        }
+    }
+}
