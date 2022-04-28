aa20a2591bd51ce464b9e19e578a0c658b5379f0
paukamisato <88379894+paukamisato@users.noreply.github.com>
2022-04-28 15:34:22 +0200

    music (#11)

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,13 +2,16 @@ package Main;
 
 
 import Classes.MainMenu;
+import Classes.MusicPlayer;
 
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
 
+        MusicPlayer.musicPlayer();
         MainMenu.showMainMenu();
+
 
     }
 }
